56a57d9c91e95321d7ae65800454a0c7e9a81a03
Ryan Silva <RyandotSilva@gmail.com>
2019-09-23 16:15:02 -0400

    Add Company constructor

--- src/Company.ts
+++ src/Company.ts
@@ -7,4 +7,13 @@ export class Company {
 		lat: number;
 		lng: number;
 	};
+
+	constructor() {
+		this.companyName = faker.company.companyName();
+		this.catchPhrase = faker.company.catchPhrase();
+		this.location = {
+			lat: parseFloat(faker.address.latitude()),
+			lng: parseFloat(faker.address.longitude())
+		};
+	}
 }
