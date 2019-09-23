52b89e45f0566da56cb3098b5cd1851d9b39e7e7
Ryan Silva <RyandotSilva@gmail.com>
2019-09-23 16:57:40 -0400

    Add map div

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
+new google.maps.Map();
